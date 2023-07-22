6a2c464e9c27f993b05cf93c19fda3de0a731ae1
Gleides Vinente <gleidevelop@gmail.com>
2023-07-22 09:04:13 -0400

    Add lib cors no projeto para expor o backend

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize-typescript';
 const connection = new Sequelize({
   dialect: 'mysql',
   host: 'localhost',
-  port: 3306,
+  port: 3320,
   username: 'root',
   password: '123456',
   database: 'lojavirtual',
