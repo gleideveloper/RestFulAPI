202bfde7eadd7a9b2c7c8e88ee1a0fc9a3e1cd0a
Gleides Vinente <gleidevelop@gmail.com>
2023-07-22 09:02:42 -0400

    Add lib cors no projeto para expor o backend

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -2,8 +2,8 @@ import { Sequelize } from 'sequelize-typescript';
 
 const connection = new Sequelize({
   dialect: 'mysql',
-  host: 'db',//'localhost',
-  // port: 3306,
+  host: 'localhost',
+  port: 3306,
   username: 'root',
   password: '123456',
   database: 'lojavirtual',
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ import setLocals from './middlewares/setLocals';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import { v4 as uuidv4 } from 'uuid';
-
+import cors from 'cors';
 const models = [VersaoDB, Usuario, TipoUsuario, Produto, Compra, CompraItem];
 
 declare module 'express-session' {
@@ -50,6 +50,7 @@ export class Api {
     }
 
     private async middleware() {
+        this.server.use(cors({credentials:true, origin: 'http://localhost:3333'}));
         this.server.use(express.json());
         this.server.use(cookieParser());
         this.server.use(setLocals);
